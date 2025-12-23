a51545ec8842cd9d98acf927e76c1b94079f811b
JEN023__ <lunar_halo_@naver.com>
2025-12-23 19:52:02 +0900

    UseEffect

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,24 @@
-import Navigation from "../../components/navigation";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [isLoading, setisLoading] = useState(true);
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setisLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
-      <Navigation />
+      {isLoading ? "Loading..." : JSON.stringify(movies)}
       <h1> home</h1>
     </div>
   );
